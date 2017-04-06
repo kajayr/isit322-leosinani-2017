f8ed502512641476748e0b7d5a162d0229e7c21f
Leo at School <kajayr_sinanian@yahoo.com>
2017-04-06 16:51:31 -0700

    first commit reactBasics

---
+++ Week01-ReactBasics/ReactBasics.js
@@ -0,0 +1,15 @@
+/**
+ * Created by bcuser on 4/6/17.
+ */
+import React from 'react';
+
+export class ReactBasics extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>An H1 element in a isit 322 Component</h1>
+            <p> Mote text </p>
+            </div>
+        )
+    }
+}
