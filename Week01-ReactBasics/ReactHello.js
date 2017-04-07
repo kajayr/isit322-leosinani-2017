334e886e7198edda950fada6f8c1a2bef412d2ad
Leo at School <kajayr_sinanian@yahoo.com>
2017-04-06 17:14:54 -0700

    ReactBasicsUpdate

---
+++ Week01-ReactBasics/ReactHello.js
@@ -0,0 +1,15 @@
+/**
+ * Created by bcuser on 4/6/17.
+ */
+import React from 'react';
+
+export class ReactHello extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>This is React Hello</h1>
+            <p> Mote text From hello page</p>
+            </div>
+        )
+    }
+}
--- Week01-ReactBasics/main.js
+++ Week01-ReactBasics/main.js
@@ -5,8 +5,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ReactBasics } from './ReactBasics.js';
+import { ReactHello} from './ReactHello.js';
 
 ReactDOM.render(
+    <div>
     <ReactBasics/>,
+    <ReactHello/>
+    </div>,
     document.getElementById('root')
 );
